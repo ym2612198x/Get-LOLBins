f15bc63b45614d86c89738592504cbef9c1c5cc4
tom <ym2612198x@proton.me>
2024-05-30 13:44:07 +0800

    Create Get-LOLBins.ps1

---
+++ Get-LOLBins.ps1
@@ -0,0 +1,40 @@
+# one-liner
+# $Category = Read-Host "Enter Category: ";$loljson = irm -Uri "https://lolbas-project.github.io/api/lolbas.json" -Method Get;foreach ($lol in $loljson){foreach ($lolcategory in $lol.Commands.Category){if ($lolcategory -eq $Category){foreach ($path in $lol.Full_Path){$fullPath = $($path.Path);if (Test-Path $fullPath){Write-Host "";Write-Host "[+] Found:`t$fullPath" -ForegroundColor Red;Write-Host "----------" -ForegroundColor DarkGray;foreach ($command in $lol.Commands) {if ($command.Category -eq $Category) {Write-Host "[*] Command:`t$($command.Command)" -ForegroundColor Yellow;Write-Host "[*] Descrip:`t$($command.Description)" -ForegroundColor Cyan;Write-Host "[*] Usecase:`t$($command.Usecase)" -ForegroundColor Magenta;Write-Host "[*] Category:`t$($command.Category)" -ForegroundColor DarkGray;}}Write-Host "----------" -ForegroundColor DarkGray}}}}}
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Category
+)
+
+$loljson = Invoke-RestMethod -Uri "https://lolbas-project.github.io/api/lolbas.json" -Method Get
+
+foreach ($lol in $loljson)
+{
+    foreach ($lolcategory in $lol.Commands.Category)
+    {
+        # if it matches our category
+        if ($lolcategory -eq $Category)
+        {
+            foreach ($path in $lol.Full_Path)
+            {
+                # test if the file exists on ourn system
+                $fullPath = $($path.Path)
+                if (Test-Path $fullPath)
+                {
+                    Write-Host ""
+                    Write-Host "[+] Found:`t$fullPath" -ForegroundColor Red
+                    Write-Host "----------" -ForegroundColor DarkGray
+                    foreach ($command in $lol.Commands) {
+                        if ($command.Category -eq $Category) {
+                            Write-Host "[*] Command:`t$($command.Command)" -ForegroundColor Yellow
+                            Write-Host "[*] Descrip:`t$($command.Description)" -ForegroundColor Cyan
+                            Write-Host "[*] Usecase:`t$($command.Usecase)" -ForegroundColor Magenta
+                            Write-Host "[*] Category:`t$($command.Category)" -ForegroundColor DarkGray
+                        }
+                    }
+                    Write-Host "----------" -ForegroundColor DarkGray
+                }
+            }
+        }
+    }
+}
